2f2a7508a50e70b1cab6149545d9c01eb69db7df
Peter Choi <jy.peter.c@gmail.com>
2022-12-09 23:19:54 -0600

    completed creating 16x16 grid, 1/1 aspect ratio

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function makeRows(rowNum){
-    for (let i = 1; i <= rowNum; i++){
-        for (let j = 1; j <= rowNum; j++){
-            var div = document.createElement("div");
-            div.style.width = "10px";
-            div.style.height = "10px";
-            div.style.background = "lightblue";
-            div.classList.add("cell");
-            document.getElementById("main").appendChild(div);
-        }
+for (let i = 1; i <= 16; i++){
+    for (let j = 1; j <= 16; j++){
+        var div = document.createElement("div");
+        div.style.background = "lightblue";
+        div.classList.add("cell");
+        document.getElementById("container").appendChild(div);
     }
-
 }
-
-makeRows(16);
