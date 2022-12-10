be6cfe5abfe849c23f3d5854f85ea7a55d8409ab
Peter Choi <jy.peter.c@gmail.com>
2022-12-09 21:07:52 -0600

    save status

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-for (let i = 1; i <= 16; i++){
-    for (let j = 1; j <= 16; j++){
-        var div = document.createElement("div");
-        div.style.background = "lightblue";
-        div.style.height = "8px";
-        div.style.width = "4px";
-        div.classList.add(i);
-        document.getElementById("main").appendChild(div);
+function makeRows(rowNum){
+    for (let i = 1; i <= rowNum; i++){
+        for (let j = 1; j <= rowNum; j++){
+            var div = document.createElement("div");
+            div.style.width = "10px";
+            div.style.height = "10px";
+            div.style.background = "lightblue";
+            div.classList.add("cell");
+            document.getElementById("main").appendChild(div);
+        }
     }
+
 }
+
+makeRows(16);
