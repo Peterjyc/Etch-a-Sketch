aec48ea6a2c7eb1e485bd67fa5b8b407cda3b414
Peter Choi <jy.peter.c@gmail.com>
2022-12-10 18:23:23 -0600

    finished core function of project, mouse hover to change color and reset button

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ for (let i = 1; i <= 16; i++){
         document.getElementById("container").appendChild(div);
     }
 }
+
+let grid = document.querySelectorAll('.cell');
+
+console.log(grid);
+
+for (let i = 0; i < grid.length; i++){
+    grid[i].addEventListener('mouseover', (event) => {
+        grid[i].style.backgroundColor = "black";
+    });
+    grid[i].addEventListener('mouseout', (event) => {
+        grid[i].style.backgroundColor = "blue";
+    });
+}
+
+function resetFunc(){
+    for (let i = 0; i < grid.length; i++){
+        grid[i].style.backgroundColor = "lightblue";
+    }
+}
